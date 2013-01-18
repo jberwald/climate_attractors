84575fca34f9b8dd44aef89696ac6e8d90274e35
Jesse Berwald <jberwald@gmail.com>
2013-01-18 11:34:53 -0500

    vineyard creator complete

--- src/python/sandbox/vineyards.py
+++ src/python/sandbox/vineyards.py
@@ -1,5 +1,5 @@
+import numpy as np
 from climate_attractors.src.python import perseus_wrap as perseus
-from numpy import loadtxt, load
 
 
 def compute_diagram_block( data, t0, length, persin=None, dtype='timeseries', **kwargs ):
@@ -30,45 +30,57 @@ def compute_diagram_block( data, t0, length, persin=None, dtype='timeseries', **
     #         persin = dfile + '_pers_'+str( t0 )+'_'+str( length )+'.txt'
     #         kwargs.update( {'output': persin} )  
 
-    data_window = data[ t0 : t0+length ]
+#    data_window = data[ t0 : t0+length ]
 
     # convert timeseries window to perseus format
-    perseus.convert2perseus( data_window, dtype=dtype, **kwargs )
+    perseus.convert2perseus( data, dtype=dtype, **kwargs )
 
-    # run perseus on the output
+    # run perseus on the output -- strip the '.txt'
     persout = persin[:-4]
     perseus.perseus( persin, persout, 'brips' ) 
     
     return persin, persout
 
-def create_vineyard( datafile, tstart, tend, wstart, wend, wsteps=10, **kwargs ):
+def create_vineyard( datafile, tstart, tend, wstart, wend, wsteps=1, debug=False, **kwargs ):
     """
     Run loops across time and over various window sizes.
 
     
     """
     fargs = { 'nsteps' : 100,
-               'bradius' : 0.01,
+              'bradius' : 0.01,
               'radius_scaling' : 1,
               'stepsize' : 0.01,
               'timeseries_steps' : 10
               }
     fargs.update( kwargs )
 
+    # Load the time series data
     try:
-        data = load( datafile )
+        data = np.load( datafile )
     except IOError:
-        data = loadtxt( datafile )
+        data = np.loadtxt( datafile )
 
+    # number of steps per unit time (eg., \delta t = 0.1)
+    t_scale = fargs['timeseries_steps']
     # indices for time range
-    for t in np.arange( tstart, tend ):
+    for t in np.arange( t_scale*tstart, t_scale*tend ):
         for w in np.arange( wstart, wend, wsteps ):
             # create the data segment
             data_window = data[ t : t + w ]
+
+            if debug:
+                print data_window
+
             # create perseus input file name
-            dfile = datafile[:-4] # also strip '.'
-            persin = dfile + '_pers_'+str( t )+'_'+str( w )+'.txt'
-            kwargs.update( {'output': persin} )  
+            prefix = datafile[:-4] # also strip '.'
+            # append t0 and window length
+            persin = prefix + '_pers_'+str( t )+'_'+str( w )+'.txt'
+
+            # update output for perseus.write_time_series(). This will be _input_ to perseus.
+            fargs.update( {'output': persin} ) 
+
+            # write the block of data to perseus format and compute persistence on said block.
             persfile = compute_diagram_block( data_window, t, w,  
                                               persin, **fargs )
 
@@ -76,7 +88,7 @@ def create_vineyard( datafile, tstart, tend, wstart, wend, wsteps=10, **kwargs )
 
 if __name__ == "__main__":
 
-    dfile = "./data/genbif0.txt"
+    #dfile = "./data/genbif0.txt"
     dfile = '/sciclone/data10/jberwald/climate_attractors/persistence/bif0/genbif0.txt'
 
     # these are multiplied by 10 later on
@@ -90,6 +102,10 @@ if __name__ == "__main__":
     radius_scaling = 1
     stepsize = 0.01
     timeseries_steps = 10 # steps sizes of 0.1
+
+
+    create_vineyard( dfile, 7000, 9000, 50, 300, 
+                     wsteps=10,timeseries_steps=1 )
 
     # for t in [ 800, 861 ]:
     #     t0 = timeseries_steps * t
